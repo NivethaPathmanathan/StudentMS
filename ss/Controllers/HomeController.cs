80c71d7e017b47541144f0c463ee741f5e6417cd
NivethaPathmanathan <47879532+NivethaPathmanathan@users.noreply.github.com>
2021-11-16 11:00:51 +0530

    home controller changed

--- ss/Controllers/HomeController.cs
+++ ss/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ss.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home";
 
             return View();
         }
